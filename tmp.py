cb700dd99fce87ad2a201324e9a6fd1d56d132e0
marcenavuc <35892378+marcenavuc@users.noreply.github.com>
2019-08-08 19:13:44 +0500

    Delete tmp.py

--- tmp.py
+++
@@ -1,9 +0,0 @@
-from keras.models import model_from_json
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("kaggle_unet.hdf5")
-print("Loaded model from disk")
