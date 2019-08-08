13c90ef9e2ae234c37aa8a4a67937856e2886c6f
marcenavuc <35892378+marcenavuc@users.noreply.github.com>
2019-08-09 01:23:37 +0500

    Added model and data generator

---
+++ utils.py
@@ -0,0 +1,96 @@
+def kaggle_unet(pretrained_weights = None,input_size = (256,256,1)):
+    inputs = Input(input_size)
+    c1 = Conv2D(16, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (inputs)
+    c1 = Dropout(0.1) (c1)
+    c1 = Conv2D(16, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (c1)
+    p1 = MaxPooling2D((2, 2)) (c1)
+
+    c2 = Conv2D(32, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (p1)
+    c2 = Dropout(0.1) (c2)
+    c2 = Conv2D(32, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (c2)
+    p2 = MaxPooling2D((2, 2)) (c2)
+
+    c3 = Conv2D(64, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (p2)
+    c3 = Dropout(0.2) (c3)
+    c3 = Conv2D(64, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (c3)
+    p3 = MaxPooling2D((2, 2)) (c3)
+
+    c4 = Conv2D(128, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (p3)
+    c4 = Dropout(0.2) (c4)
+    c4 = Conv2D(128, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (c4)
+    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+    c5 = Conv2D(256, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (p4)
+    c5 = Dropout(0.3) (c5)
+    c5 = Conv2D(256, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (c5)
+
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (u6)
+    c6 = Dropout(0.2) (c6)
+    c6 = Conv2D(128, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (c6)
+
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (u7)
+    c7 = Dropout(0.2) (c7)
+    c7 = Conv2D(64, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (c7)
+
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (u8)
+    c8 = Dropout(0.1) (c8)
+    c8 = Conv2D(32, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (c8)
+
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (u9)
+    c9 = Dropout(0.1) (c9)
+    c9 = Conv2D(16, (3, 3), activation='selu', kernel_initializer='he_normal', padding='same') (c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
+    return model
+    
+    
+def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
+                    mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
+                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (256,256),seed = 1):
+    image_datagen = ImageDataGenerator(**aug_dict)
+    mask_datagen = ImageDataGenerator(**aug_dict)
+    image_generator = image_datagen.flow_from_directory(
+        train_path,
+        classes = [image_folder],
+        class_mode = None,
+        color_mode = image_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = image_save_prefix,
+        seed = seed)
+    mask_generator = mask_datagen.flow_from_directory(
+        train_path,
+        classes = [mask_folder],
+        class_mode = None,
+        color_mode = mask_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = mask_save_prefix,
+        seed = seed)
+    train_generator = zip(image_generator, mask_generator)
+    for (img,mask) in train_generator:
+        img = img / 255.
+        mask = mask / 255.
+        mask[mask > 0.5] = 1
+        mask[mask <= 0.5] = 0
+        #img,mask = adjustData(img,mask,flag_multi_class,num_class)
+    yield (img,mask)
